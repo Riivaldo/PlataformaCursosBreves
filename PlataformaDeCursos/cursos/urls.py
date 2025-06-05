8ff56674ebded315bcf741842d6d9d5b83e9768b
1Mi2Nombre3 <jhoonn71296996@gmail.com>
2025-06-04 22:10:01 -0400

    Implementa vistas de las funcionalidades mas las urls, y formularios necesarios

--- PlataformaDeCursos/cursos/urls.py
+++ PlataformaDeCursos/cursos/urls.py
@@ -3,4 +3,7 @@ from . import views
 urlpatterns = [
     # path('', views.vistaInicial, name='inicio'),
     path('cursos/', views.lista_cursos, name='lista_cursos'),
+    path('curso/<int:curso_id>/inscribirse/', views.InscripcionACursos, name='inscripcion_curso'),
+    path('curso/<int:curso_id>/subir/', views.SubidaDeMaterias, name='subida_materias'),
+    path('inscripcion/<int:inscripcion_id>/progreso/', views.ProgresoDelEstudiante, name='progreso_estudiante'),
 ]
