1f4dcc2a02dcac8358428fc0e687b915f891604f
bogas08 <lopezgabriel0802>
2025-06-09 13:23:33 -0400

    cambiando el idioma de ingles a  esapañol

--- PlataformaDeCursos/PlataformaDeCursos/settings.py
+++ PlataformaDeCursos/PlataformaDeCursos/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'es-BO'
+#modificando el idioma a español boliviano y la hora 
+TIME_ZONE = 'America/La_Paz'
 
 USE_I18N = True
 
