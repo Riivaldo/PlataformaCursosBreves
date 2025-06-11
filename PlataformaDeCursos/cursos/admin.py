fa718084527e03c58e41b073af0f9ce842bf9d9a
AldrinRocha <160193348+AldrinRocha@users.noreply.github.com>
2025-06-11 03:20:20 -0400

    resubiendo archvios a la rama

---
+++ PlataformaDeCursos/cursos/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Profesor, Curso, Inscripcion, Recurso, Progreso
+    # ---- REGISTRO DE LOS MODELOS EN EL ADMIN ----
+# Register your models here.
+admin.site.register(Profesor)
+admin.site.register(Curso)
+admin.site.register(Inscripcion)
+admin.site.register(Recurso)
+admin.site.register(Progreso)
---
+++ PlataformaDeCursos/cursos/urls.py
@@ -0,0 +1,32 @@
+from django.urls import path
+from cursos.views import login_redirect
+from . import views
+
+urlpatterns = [
+    # path('', views.vistaInicial, name='inicio'),
+    # vista inicial, tabla de cursos
+    path('', views.lista_cursos, name='lista_cursos'),
+    # vista de detalle de cada curso
+    path("curso/<int:curso_id>/", views.detalle_curso, name="detalle_curso"),
+    # vista para la inscripción a un curso
+    path('curso/<int:curso_id>/inscribirse/', views.inscribirse_curso, name='inscribirse_curso'),
+     # vista para el registro de un usuario
+    path('registro/', views.registro_usuario, name='registro_usuario'), 
+    # vista para subir material extra a un curso
+    path('curso/<int:curso_id>/subir_material_extra/', views.subir_material_extra, name='subir_material_extra'),
+    # vista del progreso de todos los cursos
+    path('progreso/', views.progreso_estudiante, name='progreso_estudiante'),
+    # vista para ver el recurso
+    path('recurso/<int:recurso_id>/', views.ver_recurso, name='ver_recurso'),
+    # vista para ver el recurso "completar"
+    path('recurso/<int:recurso_id>/completar/', views.marcar_completado, name='marcar_completado'),
+    # vista para ver el maestro"
+    path('dashboard/', views.dashboard, name='dashboard'),
+    # vista para la redireccion entre estudiante y maestro"
+    path('redireccion/', login_redirect, name='login_redirect'),
+    # vista para perfil de usuario
+    path('perfil/', views.perfil_usuario, name='perfil_usuario'),
+]
+
+
+
