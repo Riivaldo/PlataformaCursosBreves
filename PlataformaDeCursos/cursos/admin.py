04e78d20c5886cb5893621249363a93e7105a6ec
AldrinRocha <160193348+AldrinRocha@users.noreply.github.com>
2025-06-11 03:32:13 -0400

    Delete PlataformaDeCursos directory

--- PlataformaDeCursos/cursos/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Profesor, Curso, Inscripcion, Recurso, Progreso
-    # ---- REGISTRO DE LOS MODELOS EN EL ADMIN ----
-# Register your models here.
-admin.site.register(Profesor)
-admin.site.register(Curso)
-admin.site.register(Inscripcion)
-admin.site.register(Recurso)
-admin.site.register(Progreso)
